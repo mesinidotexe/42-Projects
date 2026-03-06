5883071e4c0fe3ceebdbb4db732b2baa90f913dc
mesinidotexe <victormesini007@gmail.com>
2026-03-06 19:13:31 +0000

    py02

--- python02/ex5/ft_garden_management.py
+++ python02/ex5/ft_garden_management.py
@@ -11,26 +11,37 @@ class LightError(Exception):
 class GardenManager:
 
     def __init__(self):
-        self.gardens = []
+        self.gardens = {}
     
     def add_plant(self, name, water, light):
         if name == "":
             raise EmptyError("The name of the plant cannot be empty!")
-        self.gardens = {"water": water, "light": light}
+        self.gardens[name] = {"water": water, "light": light}
+        print(f"{name} added!")
 
-    def water_plant(self, water):
-        if water < 1 or water > 10:
-            raise WaterError("Wrong amount of water!")
 
-    def check_plant_health(self, light):
-        if light < 2 or light > 12:
-            raise LightError("Not the ideal time of sunlight!")
-
-    def errors_type(self):
+    def water_plant(self, name, water):
+        print("Opening watering sysytem")
         try:
-            self.add_plant()
-        except Exception as e:
-            print(e)
+            if water < 1 or water > 10:
+                raise WaterError(f"Wrong amount of water for {name}!")
+            print(f"Watering {name} with {water} liters")
+        finally:
+            print("Closing watering system\n")
+
+
+    def check_plant_health(self, name):
+        plant = self.gardens.get(name)
+
+        if not plant:
+            raise EmptyError(f"{name} is not in the garden")
+
+        light = plant["light"]
+
+        if light < 2 or light > 12:
+            raise LightError(f"{name} is not getting ideal sunlight")
+
+        print(f"{name} is healthy")
 
 
 def test_garden_management():
@@ -38,15 +49,27 @@ def test_garden_management():
 
     # adicionar plantas
     manager.add_plant("Rose", 5, 5)
-    manager.add_plant("", 5, 5)  # erro
+    manager.add_plant("Tulip", 5, -1)
+    try:
+        manager.add_plant("", 5, 5)  # erro
+    except EmptyError as e:
+        print(f"{e}\n")
+
 
     # regar plantas
-    manager.water_plant(5)
-    manager.water_plant(-1)  # erro
+    manager.water_plant("Rose", 5)
+    manager.water_plant("Tulip", 5)
+    try:
+        manager.water_plant("Tulip", -1)  # erro
+    except WaterError as e:
+        print(f"{e}\n")
 
     # checar saúde
-    manager.check_health("5")
-    manager.check_health("1")  # erro
+    manager.check_plant_health("Rose")
+    try:
+        manager.check_plant_health("Tulip")  # erro
+    except LightError as e:
+        print(e)
 
 if __name__ == '__main__':
     test_garden_management()
