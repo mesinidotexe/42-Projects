0d763070aba9de36d6f7f82e79fb4eb44edc4894
Victor Mesini Costa <vmesini-@c1r10s2.42lisboa.com>
2026-04-04 02:45:32 +0100

    auto

--- python05/ex2/nexus_pipeline.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List, Any, Optional, Dict, Union
-
-
-class ProcessingPipeline(ABC):
-
-    def process(self, data: Any) -> Any:
-        pass
-
