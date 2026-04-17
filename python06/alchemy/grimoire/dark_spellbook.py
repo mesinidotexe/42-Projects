09afb9e87a70425e630efccab4ee7e01cc33e769
Victor Mesini Costa <vmesini-@c1r9s13.42lisboa.com>
2026-04-17 17:23:03 +0100

    06 finished

--- python06/alchemy/grimoire/dark_spellbook.py
+++ python06/alchemy/grimoire/dark_spellbook.py
@@ -1,10 +1,12 @@
+from .dark_validator import validate_ingredients
+
+
 def dark_spell_allowed_ingredients():
     return ['bats', 'frogs', 'aresenic', 'eyeball']
 
 
-def __dark_spell_record(spell_name: str, ingredients: str):
+def dark_spell_record(spell_name: str, ingredients: str):
     spell_name.lower()
-    from .light_validator import validate_ingredients
     if 'VALID' in validate_ingredients(ingredients):
         return 'spell recorded'
     return 'spell rejected'
--- python06/alchemy/grimoire/dark_validator.py
+++ python06/alchemy/grimoire/dark_validator.py
@@ -1,6 +1,8 @@
+from .dark_spellbook import dark_spell_allowed_ingredients
+
+
 def validate_ingredients(ingredients: str):
-    from .dark_spellbook import __dark_spell_allowed_ingredients
-    allowed = __dark_spell_allowed_ingredients()
+    allowed = dark_spell_allowed_ingredients()
     for ing in allowed:
         if ing == ingredients:
             return ingredients, 'VALID'
