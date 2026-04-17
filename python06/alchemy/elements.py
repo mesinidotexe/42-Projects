a52991401cff8fddde8b2ac229ad20f0715195c9
Victor <victormesini007@gmail.com>
2026-04-17 01:01:53 +0100

    starting 06

---
+++ python06/alchemy/elements.py
@@ -0,0 +1,5 @@
+def create_earth():
+    print('Earth element created')
+
+def create_air():
+    print('Air element created')
---
+++ python06/elements.py
@@ -0,0 +1,5 @@
+def create_fire():
+    print('Fire element created')
+    
+def create_water():
+    print('Water element created')
