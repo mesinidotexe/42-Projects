46c82754b7cd6d59387c0385efdc65a3bafae359
Victor <victormesini007@gmail.com>
2026-06-24 19:33:58 +0100

    its a beginning

--- Codexion/main.c
+++ Codexion/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 
-int main(void)
+
+int *init_variables(int argc, char **argv)
 {
-    
+    int *variables;
+    int i;
+
+    variables = malloc(sizeof(int) * (argc - 1));
+    if (!variables)
+        return (NULL);
+    i = 0;
+    while(i < argc - 1)
+    {
+        variables[i] = atoi(argv[i + 1]);
+        i++;
+    }
+    if (variables)
+    return (variables);
+}
+
+
+
+int main(int argc, char **argv)
+{
+    int *variables;
+    int i;
+
+    variables = init_variables(argc, argv);
+    i = 0;
+    if (!variables)
+        return (1);
+    // Tester
+    while (variables[i])
+        printf("%d\n", variables[i++]);
+
+    free(variables);
+    return 0;
 }
