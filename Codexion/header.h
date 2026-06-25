722ea6c1c91051aaa134136db54ae3c55b55775b
Victor <victormesini007@gmail.com>
2026-06-25 21:55:08 +0100

    threads created

---
+++ Codexion/header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+# define HEADER_H
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct thread
+{
+	int index;
+}   coder;
+
+// Parser
+
+
+
+#endif
--- Codexion/main.c
+++ Codexion/main.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-
+#include "header.h"
 
 
 int *init_variables(int argc, char **argv)
@@ -18,25 +15,72 @@ int *init_variables(int argc, char **argv)
         variables[i] = atoi(argv[i + 1]);
         i++;
     }
-    if (variables)
     return (variables);
 }
 
 
 
+void *routine(void *number_of_thread)
+{
+    long id = (long)number_of_thread;
+    printf("Test from thread %ld\n", id);
+    sleep(3);
+    printf("Ending thread %ld\n", id);
+    return(NULL);
+}
+
+
+
+pthread_t *init_coders_array(int number_of_coders)
+{
+    pthread_t *coders;
+    int i;
+    int j;
+
+    coders = malloc(sizeof(pthread_t) * number_of_coders);
+    if (!coders)
+        return (NULL);
+    i = 0;
+    j = 0;
+
+    while (i < number_of_coders)
+    {
+        pthread_create(&coders[i], NULL, &routine, (void *)(long) i);
+        i++;
+    }
+
+    while (j < number_of_coders)
+    {
+        pthread_join(coders[j++], NULL);
+    }
+    
+    return (coders);
+}
+
+
 int main(int argc, char **argv)
 {
     int *variables;
-    int i;
-
+    pthread_t *coders_info;
+    
     variables = init_variables(argc, argv);
-    i = 0;
     if (!variables)
         return (1);
-    // Tester
-    while (variables[i])
-        printf("%d\n", variables[i++]);
 
+
+    // Variable Tester ===========================
+    int i;
+    i = 0;
+    while (i < argc - 1)
+        printf("%d\n", variables[i++]);
+    // ===========================================
+
+
+    // Threads Tester ============================
+    coders_info = init_coders_array(variables[0]);
+    // ===========================================
+
+    free(coders_info);
     free(variables);
     return 0;
 }
