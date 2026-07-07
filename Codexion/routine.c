fd95fffb66693181d93a092e2692b090d02e02ea
Victor <victormesini007@gmail.com>
2026-07-07 15:21:07 +0100

    routine change

--- Codexion/routine.c
+++ Codexion/routine.c
@@ -7,14 +7,6 @@ void *routine(void *arg)
     coder = (t_coder *)arg;
     usleep(coder->vars->dongle_cooldown);
     printf("Coder %d\n", coder->id);
-    printf("Number of coders %d\n", coder->vars->number_of_coders);
-    printf("Time to burnout %d\n", coder->vars->time_to_burnout);
-    printf("Time to compile %d\n", coder->vars->time_to_compile);
-    printf("Time to debug %d\n", coder->vars->time_to_debug);
-    printf("Time to refactor %d\n", coder->vars->time_to_refactor);
-    printf("Number of compiles required %d\n", coder->vars->number_of_compiles_required);
-    printf("Dongle cooldown %d\n", coder->vars->dongle_cooldown);
-    printf("scheduler %s\n", coder->vars->scheduler);
     
     printf("Ending thread\n");
 
