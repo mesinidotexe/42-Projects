0717b0c4bb5bfd9a209ccd3b967af3d1d8775262
unknown <victormesini007@gmail.com>
2026-07-20 18:21:56 +0100

    standard color put

--- Fly-in/fly_in.py
+++ Fly-in/fly_in.py
@@ -1,12 +1,33 @@
 from parse.parse import Parse
-from colorama import init
+from colorama import init, Fore
+import sys
+    
+# parameters = [nb_drones, (start_hub0 - start_hub1), start_hub_color, (end_hub0 - end_hub1), end_hub_color]
+class Main():
+    
+    @classmethod
+    def main(cls):
+        init(autoreset=True)
+        parameters: list[int | str | tuple[int | None, int | None] | None] | None = Parse.splitting_fix_lines()
+        print(parameters)
+        if not parameters:
+            print('Empty input_file or wrong input file name')
+            sys.exit(1)
+        if parameters is None or None in (parameters[0], parameters[1], parameters[3]):
+            print('Invalid syntax in one or more lines on the first 3 lines in input_file, exitting the program')
+            sys.exit(1)
 
-def main():
-    init(autoreset=True)
-    parameters: list[int | str | tuple[int | None, int | None] | None] = Parse.splitting_fix()
-    # parameters = [nb_drones, (start_hub0 - start_hub1), start_hub_color, (end_hub0 - end_hub1), end_hub_color]
-    print(parameters)
-    if None in 
+        if parameters[2] == 'outstandard':
+            parameters[2] = '\x1b[0m'
+            print('There is an outstandard color for "start_hub_color", the program will continue sticking with the regular terminal output color')
+        if parameters[4] == 'outstandard':
+            parameters[4] = '\x1b[0m'
+            print('There is an outstandard color for "end_hub_color", the program will continue sticking with the regular terminal output color')
+        print(f'{parameters[2]}{parameters}')
 
+     
 if __name__ == '__main__':
-    main()
+    try:
+        Main.main()
+    except Exception as e:
+        print(f'Failed dummy\n{e}')
--- Fly-in/parse/parse.py
+++ Fly-in/parse/parse.py
@@ -5,6 +5,11 @@ class Parse():
     
     # Auxiliar method
     @staticmethod
+    def metadata(line: str) -> bool:
+        return '[' in line or ']' in line
+
+    # Auxiliar method
+    @staticmethod
     def get_color(line: str) -> str | None:
         colors = {
             'black': Fore.BLACK,
@@ -15,12 +20,17 @@ class Parse():
             'red': Fore.RED,
             'white': Fore.WHITE,
             'yellow': Fore.YELLOW,
+            'gray': Fore.LIGHTBLACK_EX
         }
         match = re.search(r'color=(\w+)', line)
-        if match:
-            return colors.get(match.group(1))
+        if not match:
+            return None
 
-        return None
+        color_name = match.group(1)
+        if color_name not in colors:
+            return 'outstandard'
+
+        return colors[color_name]
     
     # Auxiliar method
     @staticmethod
@@ -41,61 +51,74 @@ class Parse():
         return len(stack) == 0
 
     @classmethod
-    def splitting_fix(cls):
+    def splitting_fix_lines(cls) -> list:
         variables: list[int | str | None | tuple] = []
 
-        for line in open('input_file.txt'):
+        with open('input_file.txt') as input_file:
+            for line in input_file:
 
-            if line.startswith('nb_drones'):
-                try:
-                    number_of_drones: int | None = (int(line.split(':')[1]) if len(line.split(':')) == 2 else None)
-                    variables.append(number_of_drones)
-                except ValueError:
-                    print('The amount of drones must be an integer')
-                    return None
-
-            elif line.startswith('start_hub'):
-                start_parts: list = line.split()
-                if len(start_parts) in (4, 5):
+                if line.startswith('nb_drones'):
                     try:
-                        x = int(start_parts[2])
-                        y = int(start_parts[3])
-                        start_hub: tuple[int, int] = (x, y)
-                        variables.append(start_hub)
+                        number_of_drones: int | None = (int(line.split(':')[1]) if len(line.split(':')) == 2 else None)
+                        variables.append(number_of_drones)
                     except ValueError:
-                        print('Invalid syntax on line "start_hub", args 2 and 3 must be an integer')
+                        print('The amount of drones must be an integer')
                         return None
-                else:
-                    print('Invalid syntax on line "start_hub"')
-                    return None
 
-                if '[' in line:
-                    if not cls.bracket_validator(line):
-                        print('Invalid input file, maybe you forgot to close the "[]"?')
+                elif line.startswith('start_hub'):
+                    start_parts: list = line.split()
+                    if len(start_parts) in (4, 5):
+                        try:
+                            x = int(start_parts[2])
+                            y = int(start_parts[3])
+                            start_hub: tuple[int, int] = (x, y)
+                            variables.append(start_hub)
+                        except ValueError:
+                            print('Invalid syntax on line "start_hub", args 2 and 3 must be an integer')
+                            return None
+                    else:
+                        print('Invalid syntax on line "start_hub"')
                         return None
-                start_hub_color = cls.get_color(line)
-                variables.append(start_hub_color)
 
-            elif line.startswith('end_hub'):
-                end_parts: list = line.split()
-                if len(end_parts) in (4, 5):
-                    try:
-                        x = int(end_parts[2])
-                        y = int(end_parts[3])
-                        end_hub: tuple[int, int] = (x, y)
-                        variables.append(end_hub)
-                    except ValueError:
-                        print('Invalid syntax on line "end_hub", args 2 and 3 must be an integer')
-                        return None
-                else:
-                    print('Invalid syntax on line "end_hub"')
-                    return None
+                    if cls.metadata(line):
+                        if not cls.bracket_validator(line):
+                            print('Invalid input file, maybe you forgot to close the "[]"?')
+                            return None
+                        start_hub_color = cls.get_color(line)
+                        variables.append(start_hub_color)
+                    else:
+                        variables.append(None)
 
-                if '[' in line:
-                    if not cls.bracket_validator(line):
-                        print('Invalid input file, maybe you forgot to close the "[]"?')
+                elif line.startswith('end_hub'):
+                    end_parts: list = line.split()
+                    if len(end_parts) in (4, 5):
+                        try:
+                            x = int(end_parts[2])
+                            y = int(end_parts[3])
+                            end_hub: tuple[int, int] = (x, y)
+                            variables.append(end_hub)
+                        except ValueError:
+                            print('Invalid syntax on line "end_hub", args 2 and 3 must be an integer')
+                            return None
+                    else:
+                        print('Invalid syntax on line "end_hub"')
                         return None
-                end_hub_color = cls.get_color(line)
-                variables.append(end_hub_color)
+
+                    if cls.metadata(line):
+                        if not cls.bracket_validator(line):
+                            print('Invalid input file, maybe you forgot to close the "[]"?')
+                            return None
+                        end_hub_color = cls.get_color(line)
+                        variables.append(end_hub_color)
+                    else:
+                        variables.append(None)
                 
         return variables
+    
+    # @classmethod
+    # def hubs_positions(cls):
+    #     hubs: list[dict] = []
+    #     with open('input_file.txt') as input_file:
+    #         for line in input_file:
+    #             if line.startswith('hub'):
+                    
