38ce1f883b30546cf2fd43e589d14988c6bc610a
Victor <victormesini007@gmail.com>
2026-08-20 16:12:28 +0100

    lines are drawn

--- Fly-in/teste.py
+++ Fly-in/teste.py
@@ -5,19 +5,6 @@ from colorama import Fore
 
 
 class Display():
-    
-    @staticmethod
-    def draw_connection(screen, hub: Hub, second_hub: Hub, connections: list[dict]):
-        font = pygame.font.Font(None, 25)
-        xline = font.render('-', False, 'White')
-        yline = font.render('|', False, 'White')
-        con1_x = hub.position[0] * 34 + 25
-        con1_y = hub.position[1] * 34 + 4
-        con2_x = second_hub.position[0] * 34 + 25
-        con2_y = second_hub.position[1] * 34 + 4
-           
-    
-        
 
     @staticmethod
     def create_hub():
@@ -45,11 +32,16 @@ class Display():
         elif hub.color == Fore.LIGHTBLACK_EX or hub.color == '\x1b[0m':
             surface.fill('Gray')
         
+    
+    @staticmethod
+    def zone_name(all_hubs, screen, text_font):
+
+        start_name = next((hub.name for hub in all_hubs.values() if hub.role == 'start'), 'start')
+        start_subtitle = text_font.render(start_name, False, 'White')
+        end_name = next((hub.name for hub in all_hubs.values() if hub.role == 'end'), 'end')
+        end_subtitle = text_font.render(end_name, False, 'White')
         
-    @classmethod
-    def zones(cls, surfaces, all_hubs, screen, text_font, start_subtitle, end_subtitle, symbol, underline, connections: list[dict]):
-        for surface, hub in zip(surfaces, all_hubs.values()):
-            screen.blit(surface, (hub.position[0] * 34, hub.position[1] * 34))
+        for hub in all_hubs.values():
             hub_subtitle = text_font.render(hub.name, False, 'White')
             
             if hub.role == 'start':
@@ -57,15 +49,31 @@ class Display():
             elif hub.role == 'end':
                 screen.blit(end_subtitle, (hub.position[0] * 34, hub.position[1] * 34 - 12))
             elif hub.role == 'hub':
-                screen.blit(hub_subtitle, (hub.position[0] * 34, hub.position[1] * 34 - 12))
+                screen.blit(hub_subtitle, (hub.position[0] * 34, hub.position[1] * 34 - 12))    
+    
+    
+    @staticmethod
+    def draw_line(connections, screen, all_hubs):
+        white = (255, 255, 255)
+        for hub in all_hubs.values():
             for connection in connections:
                 if connection['connection1'] == hub.name:
-                    cls.draw_connection(screen, hub, all_hubs[connection['connection2']], connections)
-
-            # if symbol != underline:
-            #     screen.blit(symbol, (hub.position[0] * 34 + 9, hub.position[1] * 34 + 6))
-            # else:
-            #     screen.blit(symbol, (hub.position[0] * 34 + 7, hub.position[1] * 34 - 2))
+                    pygame.draw.line(screen, white, (hub.position[0] * 34 + 25, hub.position[1] * 34 + 12), (all_hubs[connection['connection2']].position[0] * 34, all_hubs[connection['connection2']].position[1] * 34 + 12), 1)
+    
+        
+    @staticmethod
+    def zones(surfaces, all_hubs: dict[str, Hub], screen):
+        for surface, hub in zip(surfaces, all_hubs.values()):
+            screen.blit(surface, (hub.position[0] * 34, hub.position[1] * 34))
+        
+        
+    @staticmethod
+    def drones(all_hubs, symbol, underline, screen):
+        for hub in all_hubs.values():
+            if symbol != underline:
+                screen.blit(symbol, (hub.position[0] * 34 + 9, hub.position[1] * 34 + 6))
+            else:
+                screen.blit(symbol, (hub.position[0] * 34 + 7, hub.position[1] * 34 - 2))
         
         
     @classmethod
@@ -78,18 +86,12 @@ class Display():
         screen: pygame = pygame.display.set_mode((1600, 900))
         clock = pygame.time.Clock()
 
-        font = pygame.font.Font(None, 10)
         text_font = pygame.font.Font(None, 20)
+        font = pygame.font.Font(None, 20)
         underline = font.render('_', False, 'Black')
         pipe = font.render('|', False, 'Black')
         slash = font.render('/', False, 'Black')
         bslash = font.render('\\', False, 'Black')
-        symbols = [underline, bslash, pipe, slash]
-
-        start_name = next((hub.name for hub in all_hubs.values() if hub.role == 'start'), 'start')
-        start_subtitle = text_font.render(start_name, False, 'White')
-        end_name = next((hub.name for hub in all_hubs.values() if hub.role == 'end'), 'end')
-        end_subtitle = text_font.render(end_name, False, 'White')
         
         for hub in all_hubs.values():
             surfaces.append(cls.create_hub())
@@ -97,6 +99,8 @@ class Display():
         for surface, hub in zip(surfaces, all_hubs.values()):
             cls.associate_color(hub, surface)
 
+        symbols = [underline, bslash, pipe, slash]
+        
         frame = 0
         while True:
             for event in pygame.event.get():
@@ -107,8 +111,12 @@ class Display():
 
             symbol = symbols[(frame // 20) % 4]
 
-            cls.zones(surfaces, all_hubs, screen, text_font, start_subtitle, end_subtitle, symbol, underline, connections)
+            cls.draw_line(connections, screen, all_hubs)
+            cls.zone_name(all_hubs, screen, text_font)
+            cls.zones(surfaces, all_hubs, screen)
+            cls.drones(all_hubs, symbol, underline, screen)
             
+        
             frame += 1
             pygame.display.update()
             clock.tick(240)
